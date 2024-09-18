bacd4d50b260619fc7fa915b7a9c3b37058d6ca3
quangnp1712 <quangnp1712@gmail.com>
2024-09-18 22:14:09 +0700

    update code

--- realmen_customer_application/lib/features/presentation/auth/ui/auth_page.dart
+++ realmen_customer_application/lib/features/presentation/auth/ui/auth_page.dart
@@ -1,7 +1,5 @@
 // ignore_for_file: unused_field
 
-import 'dart:js_interop';
-
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
